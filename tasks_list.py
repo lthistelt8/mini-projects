296aa89c18f0f647291603705ee2019d82c0ef0e
lthistelt8 <luthistel09@gmail.com>
2025-05-07 19:48:11 -0400

    added docstring, fixed newline bug; added 'y' and 'n' as valid entries

--- tasks_list.py
+++ tasks_list.py
@@ -1,14 +1,16 @@
+"""Module designed to create a simple to-do list"""
+
 to_do = []
 
 while True:
     task = input('Enter a task: ')
     to_do.append(task)
     task = input('Task successfully added! Would you like to add another task? ')
-    if task.lower() == 'yes':
+    if task.lower() == 'yes' or task.lower() == 'y':
         print()
         continue
-    elif task.lower() == 'no':
+    elif task.lower() == 'no' or task.lower() == 'no':
         print('To do list ended successfully. Here is your current list of things to do. ')
         for item in to_do:
             print(item)
-        break
+            break
