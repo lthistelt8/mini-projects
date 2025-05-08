dfc8bcc8d75b073404b33c2727ec22bb55c2e6df
lthistelt8 <luthistel09@gmail.com>
2025-05-07 20:44:33 -0400

    current list items are now displayed on startup

--- tasks_list.py
+++ tasks_list.py
@@ -9,6 +9,14 @@ except json.JSONDecodeError:
     print('Warning: Could not read tasks_list.json (file may be corrupted). Starting fresh')
     to_do = []
 
+if to_do:
+    print("Here are your saved tasks:")
+    for item in to_do:
+        print("-", item)
+    print()
+else:
+    print("No saved tasks found.\n")
+
 while True:
     task = input('Enter a task: ')
     to_do.append(task)
@@ -20,7 +28,7 @@ while True:
     elif response.lower() == 'no' or response.lower() == 'n':
         print('To do list ended successfully. Here is your current list of things to do. ')
         for item in to_do:
-            print(item)
+            print("-",item)
         break
 with open("tasks_list.json", "w", encoding='utf-8') as file:
     json.dump(to_do, file)
