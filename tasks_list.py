dabc9a96078042e08f5c7fd306c5de97abfbe39a
lthistelt8 <luthistel09@gmail.com>
2025-05-14 17:36:41 -0400

    completed items will now save to the archive, and can be retrieved from the archive as well (marking it "incomplete").

--- tasks_list.py
+++ tasks_list.py
@@ -1,27 +1,36 @@
 """A simple to-do list."""
 import json
 import os
-print("Current working directory:", os.getcwd())
+print("Current working directory:", os.getcwd(),"\n")
 
 try:
     with open("tasks_list.json", "r", encoding="utf-8") as file:
-        to_do = json.load(file)
+        data = json.load(file)
+        to_do = data.get("to_do", [])
+        archive = data.get("archive", [])
 except FileNotFoundError:
+    print("WARNING: File tasks_list not found (double-check your directory). Starting fresh...")
     to_do = []
     archive = []
 except json.JSONDecodeError:
-    print('Warning: Could not read tasks_list.json (file may be corrupted). Starting fresh...')
+    print('WARNING: Could not read tasks_list.json (file may be corrupted). Starting fresh...')
     to_do = []
     archive = []
 
 while True:
     if to_do:
-        print("Here are your active tasks:")
+        active_count = 0
+
+        if active_count > 0:
+            print(f"\n Active tasks: {active_count}")
+        else:
+            print("No active tasks found.")
+
         for i, item in enumerate(to_do, 1):
             status = "C" if item['complete'] else "IN"
-            print(f"{i}. [{status}] {item['task']}")
-    else:
-        print('No saved tasks found.\n')
+            if not item['complete']:
+                active_count +=1
+                print(f"{i}. {item['task']}")
 
 
     print("\nWhat would you like to do today?")
@@ -30,7 +39,7 @@ while True:
     print ('3. View completed (archived) tasks')
     print("4. Quit")
 
-    choice = input("Enter the number (1-3) denoting your choice. ")
+    choice = input("Enter the number (1-4) denoting your choice. ")
     if choice == "1":
 
         print("\nAdding task...")
@@ -59,11 +68,11 @@ while True:
             choice = int(input("Enter the number of the task to mark complete: "))
             if 1 <= choice <= len(to_do):
                 to_do[choice - 1]["complete"] = True
-                archive.append({'task': choice})
+                archive.append({'task': to_do[choice - 1]})
                 print(f"\nMarked '{to_do[choice - 1]['task']}' as complete. Archive updated.")
 
                 with open("tasks_list.json", "w",encoding="utf-8") as file:
-                    json.dump(to_do, file)
+                    json.dump({ "to_do": to_do, "archive": archive}, file)
             else:
                 print("\nInvalid task number.")
         except ValueError:
@@ -73,14 +82,23 @@ while True:
     elif choice == "3":
         print("\nCompleted tasks: ")
         for i, item in enumerate(archive, 1):
-            if status == "C":
-                print(status)
-        revers = "Enter the number of the task to return to 'incomplete' status. Otherwise, press the Space bar to return to the main menu."
+            if item['task']['complete']:
+                print(f"{i}. {item['task']['task']}")
+        revers =int(input("Enter the number of the task to return to 'incomplete' status. Otherwise, press 0 to return to the main menu. "))
         if 1 <= revers <=len(archive):
-            archive[revers - 1]["complete"] = False
-        elif revers == " ":
-            print("\nReturning to main menu...")
+            archive[revers - 1]['task']["complete"] = False
+            to_do.append(archive[revers - 1]['task'])
+            archive.pop(revers - 1)
+            with open("tasks_list.json", "w", encoding="utf-8") as file:
+                json.dump({"to_do": to_do, "archive": archive}, file)
+            print('Task has been returned to active list.')
+        elif revers == 0:
+            print("\nReturning to main menu...\n")
             continue
+        else:
+            print("\nInvalid key. Please select the number corresponding with the task, or press 0 to return to the main menu.")
+            continue
+        continue
 
 
     elif choice == "4":
@@ -98,4 +116,4 @@ while True:
             print("Admin action cancelled. Returning to main menu.\n")
 
     else:
-        print("Invalid input. Please enter a number between 1-3.")
+        print("Invalid input. Please enter a number between 1-4.")
